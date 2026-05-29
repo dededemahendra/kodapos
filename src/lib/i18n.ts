a769287aad4d8841e6cf19cbc22ed2fadb73b63b
Dede Mahendra <dededemahendra@gmail.com>
2026-05-29 16:13:08 +0800

    feat(i18n): activate persisted locale on startup

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,8 +1,9 @@
 import { i18n } from '@lingui/core';
+import { getStoredLocale } from '~/lib/locale';
 import { messages as enMessages } from '~/locales/en/messages.po';
 import { messages as idMessages } from '~/locales/id/messages.po';
 
 i18n.load({ id: idMessages, en: enMessages });
-i18n.activate('id');
+i18n.activate(getStoredLocale());
 
 export { i18n };
